52446471691921362e78035bb0e5293d2a964da3
Carla <carladmaximo@gmail.com>
2022-03-02 20:16:33 -0300

    ajustes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import '../style/home.css';
 
 export default class Home extends Component {
+  // constructor() {
+  //   super();
+    
+  //   function dinamicText() {
+  //     // const textArr = ['O', 'l', 'á',',', ' ', 'm', 'e', 'u',' ', 'n', 'o', 'm', 'e', ' ', 'é', ' ', 'C', 'a', 'r', 'l', 'a'];
+  //     // setTimeout(() => {
+  //     //   console.log('running') 
+  //     //   return (<h1>qualquer coisa</h1>)
+  //     // }, 1000);
+  //     return <h1>HEY!</h1>
+  // }
+  // componentDidMount() {
+  //   this.dinamicText()
+  // }
+  // }
   render() {
     return (
       <div className="div-main"  id="home">
@@ -9,6 +24,7 @@ export default class Home extends Component {
           <div className="picture-profile"></div>
           <div className="box-about">
             <h2 className="about-title">Olá, eu sou a Carla!</h2>
+            {/* { this.dinamicText()} */}
             <p>Estudo programação há aproximadamente 1 ano e procuro pela minha primeira experiência profissional como Desenvolvedora Front-End Junior</p>
             <div className="skills">
               <i class="html5 big icon"></i>
