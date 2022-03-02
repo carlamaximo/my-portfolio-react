503f9c56ec2546d088f423b938e8c649e275fdbd
Carla <carladmaximo@gmail.com>
2022-03-02 18:56:31 -0300

    algumas alterações

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ export default class Contact extends Component {
     return (
       <div className="div-principal-contatos">
         <nav id="bottom">
-            <a href="https://github.com/carlamaximo">
+            <a href="https://www.linkedin.com/in/carlamaximo/">
               <svg className="icon-bootstrap" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                 <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
               </svg>
@@ -21,10 +21,10 @@ export default class Contact extends Component {
               </svg>
             </a>
           </nav>
-          <p className="paragraph-footer">Feito com React e  <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
+          {/* <p className="paragraph-footer">Feito com React e  <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
             <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
           </svg>
-        </p>
+        </p> */}
       </div>
     )
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default class Header extends Component {
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16">
             <path className="icon-heart" d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/>
             </svg>
-            <h2 className="header-name">Carla</h2>
+            <h2 className="header-name">Carla Máximo</h2>
           </div>
           <nav className="menu">
             <a className="menu-item" href="#home">Home</a>
