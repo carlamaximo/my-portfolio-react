bc9641785e40faf274937b625fb9e8fd20db1461
Carla <carladmaximo@gmail.com>
2022-03-03 22:53:01 -0300

    mais uma versao

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,8 @@ export default class About extends Component {
           <h2 className="subtitle">Quem é Carla Máximo?</h2>
        </div>
        <p className="paragraph-about">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange</p>
+       <br />
+       <p className="paragraph-about">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange</p>
       <div>
       </div>
       </div>
